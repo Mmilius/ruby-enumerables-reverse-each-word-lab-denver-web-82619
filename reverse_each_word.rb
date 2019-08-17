8df306d994afe81971a4a3497d42d8ea4f1e9e05
Martin Milius <martin.milius@gmail.com>
2019-08-17 03:05:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_sentence = new_sentence.split(" ")
+  new_sentence = sentence.split(" ")
   reversed_sentence = new_sentence.each {|w| w.reverse!}
   return reversed_sentence.join(" ")
   end
