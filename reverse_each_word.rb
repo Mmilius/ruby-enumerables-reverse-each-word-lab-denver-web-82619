e4810b0fe9d0302bdc914d941d9553fce9a7e437
Martin Milius <martin.milius@gmail.com>
2019-08-17 02:48:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(string)
-  sentence1 = []
-  sentence2 = []
- stentence1.reverse = string.split(" ")
-sentence2.reverse = string.split (" ")
+def reverse_each_word(sentence1)
+ sentence1 = sentence1.split(" ")
+ sentence1 = []
+ 
+
 
 .collect
 end
